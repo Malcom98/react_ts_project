0956d455e8f0e5f117c4a02ef26cda4fcd2a5ff3
dino <dinosmotivationalchannel@gmail.com>
2022-02-08 19:26:40 +0100

    Application

--- ts_react_app/src/components/InputField.tsx
+++ ts_react_app/src/components/InputField.tsx
@@ -1,13 +1,15 @@
+import React from "react";
 import "./styles.css";
 
 interface Props {
   todo: string;
   setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
 }
 
-const InputField = ({ todo, setTodo }: Props) => {
+const InputField = ({ todo, setTodo, handleAdd }: Props) => {
   return (
-    <form className="input">
+    <form className="input" onSubmit={handleAdd}>
       <input
         type="input"
         placeholder="Enter a task"
---
+++ ts_react_app/src/components/SingleTodo.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Todo } from "../model";
+import { AiFillEdit, AiFillDelete } from "react-icons/ai";
+import { MdDone } from "react-icons/md";
+
+interface Props {
+  todo: Todo;
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+}
+
+const SingleTodo = ({ todo, todos, setTodos }: Props) => {
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+
+  return (
+    <form className="todos_single">
+      {todo.isDone ? (
+        <s className="todos__Single--text">{todo.todo}</s>
+      ) : (
+        <span className="todos__Single--text">{todo.todo}</span>
+      )}
+      <div>
+        <span className="icon" onClick={() => handleDelete(todo.id)}>
+          <AiFillDelete />
+        </span>
+        <span className="icon" onClick={() => handleDone(todo.id)}>
+          <MdDone />
+        </span>
+      </div>
+    </form>
+  );
+};
+
+export default SingleTodo;
